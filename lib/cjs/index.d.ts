22e3467425a3fa25725045a94c78eb6255b525d0
siahgoodwork <83395882+siahgoodwork@users.noreply.github.com>
2022-12-01 14:59:57 +0800

    chore: build

--- lib/cjs/index.d.ts
+++ lib/cjs/index.d.ts
@@ -1,9 +1,15 @@
-declare function useRepeatable<T>({ initialState, newItem, }: {
+import { RepeatableList } from "./repeatable-list";
+declare function useRepeatable<T>({ initialState, newItem, onChange, }: {
     initialState?: T[];
     newItem: T;
+    onChange?: (items: T[]) => void;
 }): {
-    items: T[];
-    addItem: (item?: T | null) => void;
+    items: (T & {
+        id: string;
+    })[];
+    addItem: (item?: T, n?: number) => void;
     removeItem: (n?: number) => void;
+    moveItem: (from: number, to: number) => void;
+    updateItem: (n: number, item: T) => void;
 };
-export { useRepeatable };
+export { useRepeatable, RepeatableList };
