c54daea44feb01f1a7af7d4f0c86f299a100195c
bryangoodwork <bryan@goodwork.sg>
2022-12-20 16:49:10 +0800

    fix: type error, removed old build files, updated version

--- lib/esm/components/AddItemButton.d.ts
+++
@@ -1,9 +0,0 @@
-import { FC, MouseEventHandler } from "react";
-import { AddItem } from "../types";
-export declare const AddItemButton: <T extends object>({ CustomComponent, addItem, }: {
-    CustomComponent: FC<{
-        className?: string | undefined;
-        onClick: MouseEventHandler<HTMLButtonElement>;
-    }> | undefined;
-    addItem: AddItem<T>;
-}) => JSX.Element;
