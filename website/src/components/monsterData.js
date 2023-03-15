a9b229fa5cbed1e2225a6446f9bb86157a24b79d
Corrinne <musselmammary@gmail.com>
2023-03-15 16:06:10 +0800

    feat: WIP

---
+++ website/src/components/monsterData.js
@@ -0,0 +1,39 @@
+const monsterData = [
+  {
+    name: "Potamus",
+    power: "Sword Crusher",
+    type: "Heavyweight",
+    number: "001",
+    image: "/img/mon-potamus.svg",
+  },
+  {
+    name: "Xpelyarmus",
+    power: "Belching Glitter",
+    type: "Aggressive",
+    number: "025",
+    image: "/img/mon-xpel.svg",
+  },
+  {
+    name: "GOAT",
+    power: "Multi-Tasker",
+    type: "Work Horse",
+    number: "120",
+    image: "/img/mon-goat.svg",
+  },
+  {
+    name: "Batty Boo",
+    power: "Echolocation",
+    type: "Office Gossiper",
+    number: "258",
+    image: "/img/mon-batty.svg",
+  },
+  {
+    name: "Unknown",
+    power: "Mass Destruction",
+    type: "Evil Child",
+    number: "666",
+    image: "/img/mon-unknown.svg",
+  },
+];
+
+export { monsterData };
