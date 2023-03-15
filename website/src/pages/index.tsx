997c101ff64865da97252718cb9a229e48df890c
Corrinne <musselmammary@gmail.com>
2023-03-15 16:07:46 +0800

    feat: wip

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -5,13 +5,81 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import styles from "./index.module.css";
 import { featuresData } from "../components/featuresData.js";
+import { monsterData } from "../components/monsterData.js";
 import "animate.css";
 import { RepeatableList } from "@thegoodwork/use-repeatable";
-
 import { CopyBlock, CodeBlock, a11yLight, a11yDark } from "react-code-blocks";
+import { useState } from "react";
+import { filter } from "lodash";
+
+type Monster = {
+  name: string;
+  power: string;
+  type: string;
+  number: string;
+  image: string;
+};
+
+// para monsters = user created array of monsters
+// m = individual monster in the monsters array
+// the sequencer function looks for monsters that are NOT present in the user created array
+// and put this list of monsters in an array called filteredMonsters
+// it then returns the 1st monster that is in the filteredMonsters list
+
+function randomizer(monsters: Monster[]) {
+  const randomIndex = Math.floor(Math.random() * monsters.length);
+
+  if (randomIndex === monsters.length) {
+    return monsters[randomIndex - 1];
+  }
+  // floor of (0/1 * 4(no. of monsters index)-1)
+
+  console.log(monsters[randomIndex], randomIndex);
+  return monsters[randomIndex];
+
+  //   const filteredMonsters = monsterData.filter((m) => {
+  //     return monsters.indexOf(m) === -1;
+  //   });
+  //   return filteredMonsters[0];
+}
 
 export default function Home(): JSX.Element {
+  const [monsters, setMonsters] = useState<(Monster & { id: string })[]>([]);
+  // when things are added into the repeater, it will generate a new id for the item added
+  const [disable, setDisable] = useState(false);
+
+  // newItem={sequencer()}
+
+  // const arrOfMonsters = monsters;
+  // const uniqueMon = arrOfMonsters.filter((item, index) => {
+  //   return arrOfMonsters.indexOf(item) === index;
+  // });
+
+  const monster: Monster = {
+    name: "Potamus",
+    power: "Sword Crusher",
+    type: "Heavyweight",
+    number: "001",
+    image: "/img/mon-potamus.svg",
+  };
+  //default new item: default 1st monster, empty state of sort
+  // add randomizer, utility function to initialize, on the Recruit button, or standard sequence
+
+  const initialMonster = [
+    {
+      name: "Potamus",
+      power: "Sword Crusher",
+      type: "Heavyweight",
+      number: "001",
+      image: "/img/mon-potamus.svg",
+    },
+  ];
+
   const { siteConfig } = useDocusaurusContext();
+  // const { items, removeItem, addItem, moveItem } = useRepeatable({
+  //   newItem: monster,
+  //   initialState: initialMonster,
+  // });
   return (
     <Layout
       title={`useRepeater ${siteConfig.title}`}
@@ -27,10 +95,10 @@ export default function Home(): JSX.Element {
                     One <span className={styles.coloredh1}>hook</span> to{" "}
                     <span className={styles.coloredh1}>repeat</span> them all
                   </h1>
-                  <h3>
+                  <h2>
                     A plug-and-play React library to create repeatable fields
                     effortlessly.
-                  </h3>
+                  </h2>
                 </div>
 
                 <div className={styles.mascot}>
@@ -97,22 +165,78 @@ npm run start`}
 
               <section className={styles.demo}>
                 <div className={styles.demoLayout}>
-                  <RepeatableList
-                    initialState={[]}
-                    newItem={{ name: "Apple" }}
-                    Card={({ DragHandle }) => (
-                      <div>
-                        <DragHandle>=</DragHandle>this is a Card
-                      </div>
-                    )}
-                    Layout={({ Cards, AddButton }) => (
-                      <div id="demo-list" className={styles.demoList}>
-                        {Cards}
-                        <hr />
-                        <AddButton>Test</AddButton>
-                      </div>
-                    )}
-                  />
+                  <div className={styles.repeatableContainer}>
+                    <RepeatableList
+                      onChange={(items) => {
+                        setMonsters(items);
+                      }}
+                      initialState={[]}
+                      newItem={randomizer(monsterData)}
+                      Card={({
+                        DragHandle,
+                        item,
+                        items,
+                        index,
+                        removeItem,
+                        addItem,
+                        moveItem,
+                      }) => (
+                        <div>
+                          <div className={styles.cardButtons}>
+                            <div>
+                              <DragHandle>
+                                <img
+                                  src="./img/drag-handle.svg"
+                                  alt="drag handle"
+                                  className={styles.dragHandle}
+                                />
+                              </DragHandle>
+                            </div>
+                            <div>
+                              <MonsterCard monsterData={item} />
+                            </div>
+
+                            <button
+                              onClick={() => removeItem(index)}
+                              className={styles.removeButton}
+                            >
+                              <img
+                                src="./img/remove-button.svg"
+                                alt="Remove Monster"
+                              />
+                            </button>
+                          </div>
+
+                          {/* {monsterData.map((monsterData) => {
+                            return ();
+                          })} */}
+                        </div>
+                      )}
+                      Layout={({ Cards, items, addItem }) => {
+                        if (items.length >= 5) {
+                          setDisable(true);
+                        } else {
+                          setDisable(false);
+                        }
+
+                        return (
+                          <div id="demo-list" className={styles.demoList}>
+                            {Cards}
+                            <button
+                              className={styles.addButton}
+                              disabled={disable}
+                              onClick={() => {
+                                addItem();
+                              }}
+                            >
+                              Recruit More Monsters!
+                            </button>
+                          </div>
+                        );
+                      }}
+                    />
+                  </div>
+
                   <div className={styles.trialElements}>
                     <img
                       src="./img/fireball.svg"
@@ -147,8 +271,8 @@ npm run start`}
                   </div>
                   <div className={styles.demoArea}>
                     <div className={styles.demoHeading}>
-                      <h4>Trial of The Repeater</h4>
-                      <p>Test It Out!</p>
+                      <h3>Castle Staff List</h3>
+                      <p>Non-scary Demo</p>
                     </div>
                     <div className={styles.demoWindow}>
                       <img src="./img/demo-area.svg" alt="demo area" />
@@ -188,7 +312,7 @@ npm run start`}
                 />
 
                 <div className={styles.codeBlockBase}>
-                  <h4>Using The Hook</h4>
+                  <h3>Using The Hook</h3>
 
                   <div className={styles.codeBlock}>
                     <CodeBlock
@@ -212,7 +336,7 @@ npm run start`}
                         boxShadow: "none",
                         background: "white",
                       }}
-                      text={`import {useRepeatable} from 'use-repeatable'
+                      text={`import {useRepeatable} from '@thegoodwork/use-repeatable'
 
 type Fruit = { color: string; name: string }; 
                 
@@ -259,7 +383,7 @@ const { items, removeItem, addItem, moveItem } = useRepeatable({
                         margin: 0,
                         boxShadow: "none",
                       }}
-                      text={`import {useRepeatable} from 'use-repeatable'
+                      text={`import {useRepeatable} from '@thegoodwork/use-repeatable'
 
 type Fruit = { color: string; name: string }; 
                 
@@ -289,7 +413,7 @@ const { items, removeItem, addItem, moveItem } = useRepeatable({
 
               <div className={styles.codeBlockContainer}>
                 <div className={styles.codeBlockBase}>
-                  <h4>Using The Component</h4>
+                  <h3>Using The Component</h3>
 
                   <div className={styles.codeBlock}>
                     <CodeBlock
@@ -397,7 +521,7 @@ return <RepeatableList
           >
             <div className={styles.featuresLayout}>
               <div className={styles.featuresContainer}>
-                <h4>Features</h4>
+                <h3>Features</h3>
                 <img src="/img/jelly.svg" className={styles.jelly} />
                 <img
                   src="/img/darkmode/jelly-dm.svg"
@@ -426,7 +550,7 @@ return <RepeatableList
           >
             <div className={styles.learnMoreLayout}>
               <div className={styles.learnMoreText}>
-                <h2>Adventure Awaits!</h2>
+                <h1>Adventure Awaits!</h1>
                 <p>
                   There is so much more to learn about useRepeatable, read our
                   documentation or visit our repository to discover more!
@@ -457,5 +581,73 @@ function FeatureCard({ data }) {
     </div>
   );
 }
+// when theres object involve, use { monsterData }: { monsterData: Monster },
+// { monsterData } has the type of { monsterData: Monster }, which in turns have its own specificied type within the object;  Monster
+function MonsterCard({ monsterData }: { monsterData: Monster }) {
+  return (
+    <div className={styles.monsterCard}>
+      <div className={styles.monsterCardBackground}>
+        <div className={styles.monsterImage}>
+          <img src={monsterData.image} alt={monsterData.name} />
+        </div>
+        <div className={styles.monsterInfo}>
+          <div className={styles.monsterIndex}>
+            <h6>Monster Information</h6>
+            <h5>{monsterData.number}</h5>
+          </div>
+          {/* <hr className={styles.horizontalLine}/> */}
+
+          <div className={styles.monsterName}>
+            <h5>{monsterData.name}</h5>
+            <h6>Name</h6>
+          </div>
+
+          <div className={styles.monsterPower}>
+            <h5>{monsterData.power}</h5>
+            <h6>Power</h6>
+          </div>
+
+          <div className={styles.monsterType}>
+            <h5>{monsterData.type}</h5>
+            <h6>Type</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 // className={clsx("col col--10", styles.section)
+
+///////////////////////////////////////// no duplicates /////////////////
+// how to use array.find() / array.filter() to check if the monster already exists (if remove button is used)
+// indexOf()
+
+// if index of monster is greater than -1, it means the monster is present.
+
+// if a specific MonsterCard is present, then when the <AddButton/> is clicked again, it should skip that index and add the next index in line
+
+// const MonstersPresent = MonstersAvailable.filter(MonstersAvailable => index > -1)
+
+// Disabler
+// How to: Disable recuit monster button if <MonsterCard/>.length >= 5
+// if <MonsterCard/>.length = 5, <AddButton/> disabled = true
+// if (<MonsterCard/>.length = 5)? <AddButton/> disabled : <AddButton/> enabled
+//  measure length of clicks? if <AddButton/> clicked = 5, disable button
+
+// function disabler(items) {
+//   items.length >= 5 ? disable : null;
+//   // if (items.length >= 5) {
+//   //   return (disableButton = true);
+//   // } else {
+//   //   return (disableButton = false);
+//   // }
+// }
+
+function Button({ AddButton }) {
+  return (
+    <>
+      <button>Recruit More Monsters!</button>
+    </>
+  );
+}
